f9d6af1b90bcf65da4f93d342815f0d8ca283e16
trmcreates <admin@payperyou.com>
2022-09-15 08:50:36 +0300

    new app todo models for project and todo

---
+++ todo/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from authapp.models import User
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=128)
+    users = models.ManyToManyField(User)
+    repository = models.URLField(verbose_name="Репозиторий", blank=True)
+
+
+class Todo(models.Model):
+    text = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField(auto_now_add=True)
+    users = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+
