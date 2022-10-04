cbedf23f18fc07b62beda647f82e6290c4a2c1d3
trmcreates <admin@payperyou.com>
2022-10-04 15:47:21 +0300

    JWT

--- notes/urls.py
+++ notes/urls.py
@@ -6,6 +6,8 @@ from rest_framework.routers import DefaultRouter
 from authapp.views import UserCustomViewSet
 from todo.views import ProjectModelViewSet, TodoModelViewSet
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 router = DefaultRouter()
 router.register('users', UserCustomViewSet)
 router.register('projects', ProjectModelViewSet),
@@ -16,5 +18,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api-token/', obtain_auth_token),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include(router.urls)),
 ]
